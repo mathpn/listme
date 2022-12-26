d33453eca8da465d9c387de3e93212724e4a89fc
Matheus Pedroni <pnmatheus@protonmail.com>
2022-12-26 15:53:14 -0300

    print file summary by default and optionally skip it

--- src/rememberme.py
+++ src/rememberme.py
@@ -10,25 +10,11 @@ import subprocess
 from rich import print
 from rich.columns import Columns
 from rich.console import Console
+from rich.padding import Padding
 from rich.text import Text
 
 from git_tools import blame_lines
 
-TAGS = ["TODO", "FIXME", "XXX", "NOTE", "BUG", "OPTIMIZE"]
-# pcre2
-REGEX = (
-    "^\s*(?:(?:#+|\/\/+|<!--|--|\/\*|\"\"\"|''')\s*)*\s*"
-    + f"(?:^|\\b)({'|'.join(TAGS)})[\s:;-]+(.+?)"
-    + "(?=$|-->|#\}\}|\*\/|--\}\}|\}\}|#+|#\}|\"\"\"|''')"
-)
-RG_REGEX = (
-    "^\s*(?:(?:#+|//+|<!--|--|/\*|\"\"\"|''')\s*)*\s*"
-    + f"(?:^|\\b)({'|'.join(TAGS)})[\s:;-]+(.+?)"
-    + "(?:$|-->|#\}\}|\*/|--\}\}|\}\}|#+|#\}|\"\"\"|''')"
-)
-# print(REGEX)
-# REGEX = f"({'|'.join(TAGS)}) (.*)"
-REGEX_TAGS = f"({'|'.join(TAGS)})"
 INLINE_REGEX = (
     "^\s*(?:(?:#+|\/\/+|<!--|--|\/\*|\"\"\"|''')\s?)*|(?:-->|#}}|\*\/|--}}|}}|#+|#}|\"\"\"|''')*$"
 )
@@ -38,7 +24,7 @@ console = Console()
 
 
 def boldify(string: str) -> str:
-    return re.sub(REGEX_TAGS, r"[bold]\1[/bold]", string)
+    return f"[bold]{string}[/bold]"
 
 
 # XXX this is just temporary
@@ -46,7 +32,6 @@ def emojify(string: str) -> str:
     return ":no_entry: " + string
 
 
-# TODO summary per file
 def parse_rg_output(output: str) -> dict[str, list]:
     lines = output.splitlines()
     by_file = {}
@@ -69,24 +54,38 @@ def pad_line_number(number: str, max_digits: int) -> str:
     return number + " " * (max_digits - len(number))
 
 
+def prettify_summary(file_summary: dict[str, int], padding: int = 0) -> str:
+    return Padding(
+        " ".join(
+            f"{emojify(boldify(tag))}: {count}" for tag, count in file_summary.items() if count > 0
+        ),
+        pad=(0, 0, 0, padding + 2),
+    )
+
+
 # TODO colorful + emojis
-def print_parsed_output(by_file: dict[str, list]) -> None:
+def print_parsed_output(
+    by_file: dict[str, list], tags: list[str], regex: re.Pattern, args: argparse.Namespace
+) -> None:
     files = sorted(by_file)
     for file in files:
-        print(f"\n{file}")
+        print_lines = []
+        tag_counter = {tag: 0 for tag in tags}
         contents = by_file[file]
         lines = contents["lines"]
         texts = contents["texts"]
         blames = blame_lines(file, list(map(int, lines)))
         max_digits = max(len(line_n) for line_n in lines)
+        print(f"\n{file} ({len(lines)} comments):")
         for i, text in enumerate(texts):
-            matches = re.search(REGEX, text)
+            matches = re.search(regex, text)
             if not matches:
                 raise ValueError(f"something went wrong! -> {text}")  # FIXME remove this
             groups = matches.groups()
             if len(groups) != 2:
                 raise ValueError(f"something went wrong! -> {text}: {groups}")  # FIXME remove this
             tag, txt = groups
+            tag_counter[tag] += 1
             git_author, git_date = blames[i]
             line = (
                 pad_line_number(lines[i], max_digits)
@@ -96,20 +95,46 @@ def print_parsed_output(by_file: dict[str, list]) -> None:
                 + re.sub(INLINE_REGEX, "", txt).strip()  # FIXME deactivate auto-highlighting
             )
             columns = Columns([line, git_author], width=console.width // 2 - 1, expand=True)
-            print(columns)
+            print_lines.append(columns)
+        if args.summary:
+            print(prettify_summary(tag_counter, padding=max_digits))
+        for line in print_lines:
+            print(line)
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("folder", nargs="?", type=str, default=os.getcwd())
+    parser.add_argument(
+        "--tags", "-T", nargs="+", default=["TODO", "FIXME", "XXX", "NOTE", "BUG", "OPTIMIZE"]
+    )
+    summary_group = parser.add_mutually_exclusive_group()
+    summary_group.add_argument(
+        "--summary", "-s", action="store_const", dest="summary", const=True, default=True
+    )
+    summary_group.add_argument(
+        "--no-summary", "-S", action="store_const", dest="summary", const=False
+    )
     args = parser.parse_args()
 
-    print(" ".join(["./bin/rg", REGEX, args.folder, "-n", "--pcre2"]))
+    TAGS = sorted(args.tags)
+    # pcre2
+    REGEX = re.compile(
+        "^\s*(?:(?:#+|\/\/+|<!--|--|\/\*|\"\"\"|''')\s*)*\s*"
+        + f"(?:^|\\b)({'|'.join(TAGS)})[\s:;-]+(.+?)"
+        + "(?=$|-->|#\}\}|\*\/|--\}\}|\}\}|#+|#\}|\"\"\"|''')"
+    )
+    RG_REGEX = (
+        "^\s*(?:(?:#+|//+|<!--|--|/\*|\"\"\"|''')\s*)*\s*"
+        + f"(?:^|\\b)({'|'.join(TAGS)})[\s:;-]+(.+?)"
+        + "(?:$|-->|#\}\}|\*/|--\}\}|\}\}|#+|#\}|\"\"\"|''')"
+    )
+
     rg_output = subprocess.check_output(["./bin/rg", RG_REGEX, args.folder, "-n"])
-    #rg_output = subprocess.check_output(["./bin/rg", REGEX, args.folder, "-n", "--pcre2"])
+    # print(" ".join(["./bin/rg", RG_REGEX, args.folder, "-n"]))
     rg_output = rg_output.decode("utf-8")
     by_file = parse_rg_output(rg_output)
-    print_parsed_output(by_file)
+    print_parsed_output(by_file, TAGS, REGEX, args)
 
 
 if __name__ == "__main__":
