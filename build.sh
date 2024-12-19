d49bf1d9bc8869551dd618a106067fc7f93003e1
Matheus Pedroni <pnmatheus@protonmail.com>
2024-12-19 12:03:44 -0300

    update build script

--- build.sh
+++ build.sh
@@ -1,17 +1,61 @@
 #!/bin/bash
 
-architectures=("amd64" "arm64")
-goos=("linux" "darwin" "windows")
+# From: https://github.com/akrabat/rodeo/blob/main/build-executables.sh
+# From: https://stackoverflow.com/a/53583797/23060
+# From: https://gist.github.com/DimaKoz/06b7475317b12e7ffa724ef0e115a4ec
 
-mkdir ./bin/
-for arch in ${architectures[@]}; do
-	for os in ${goos[@]}; do
-		echo "building for $os/$arch"
-		env GOOS=$os GOARCH=$arch go build .
-		if [[ $os = "windows" ]]; then
-			zip -m ./bin/listme_${os}_${arch}.zip ./listme.exe
-		else
-			zip -m ./bin/listme_${os}_${arch}.zip ./listme
-		fi
-	done
+version=$1
+if [[ -z "$version" ]]; then
+	echo "usage: $0 <version>"
+	exit 1
+fi
+package_name=listme
+
+# The full list of the platforms is at: https://golang.org/doc/install/source#environment
+platforms=(
+	"darwin/amd64"
+	"darwin/arm64"
+	"linux/amd64"
+	"linux/arm"
+	"linux/arm64"
+	"windows/amd64"
+)
+
+rm -rf release/
+mkdir -p release
+
+for platform in "${platforms[@]}"; do
+	platform_split=(${platform//\// })
+	os=${platform_split[0]}
+	GOOS=${platform_split[0]}
+	GOARCH=${platform_split[1]}
+
+	if [ $os = "darwin" ]; then
+		os="macOS"
+	fi
+
+	output_name=$package_name'-'$version'-'$os'-'$GOARCH
+	zip_name=$output_name
+	if [ $os = "windows" ]; then
+		output_name+='.exe'
+	fi
+
+	echo "Building release/$output_name..."
+	env GOOS=$GOOS GOARCH=$GOARCH go build \
+		-ldflags "-X github.com/mathpn/listme.Version=$version" \
+		-o release/$output_name
+	if [ $? -ne 0 ]; then
+		echo 'An error has occurred! Aborting the script execution...'
+		exit 1
+	fi
+
+	pushd release >/dev/null
+	if [ $os = "windows" ]; then
+		zip $zip_name.zip $output_name
+		rm $output_name
+	else
+		chmod a+x $output_name
+		gzip $output_name
+	fi
+	popd >/dev/null
 done
